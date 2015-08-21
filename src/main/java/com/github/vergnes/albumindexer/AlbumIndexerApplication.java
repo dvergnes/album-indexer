208a073de75bd259ff62590bd4ef25d75ff13dd6
Denis Vergnes <Denis.Vergnes@akqa.com>
2015-08-21 22:20:34 +0800

    Initial import

---
+++ src/main/java/com/github/vergnes/albumindexer/AlbumIndexerApplication.java
@@ -0,0 +1,12 @@
+package com.github.vergnes.albumindexer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumIndexerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlbumIndexerApplication.class, args);
+    }
+}
